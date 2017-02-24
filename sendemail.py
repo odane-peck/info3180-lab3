fbda2dc5c0f177be99e74a57ae79f2d497d8e461
Odane E Peck <odane.peck@mymona.uwi.edu>
2017-02-24 01:44:35 +0000

    inital commit

---
+++ sendemail.py
@@ -0,0 +1,53 @@
+import smtplib
+
+fromaddr = 'odane.peck24@gmail.com'
+
+toaddr  = ['odane.peck24@gmail.com']
+
+#message = """From: {} <{odane@gmail.com}>
+
+#To: {To Person} <{odane.com}>
+
+#Subject: {SMTP e-mail test}
+
+#{}"""
+message = """Love is beautiful"""
+Subject = "To everyone"
+messagetosend = message.format(
+                             fromaddr,
+
+
+                             toaddr,
+
+
+                             message)
+
+# Credentials (if needed)
+
+username = 'odane.peck24@gmail.com'
+
+password = 'tptf uejc vfhb koku'
+
+
+# Prepare actual message
+
+message = """\
+fromaddr: %s
+toaddr: %s
+messagetosend: %s
+%s
+""" % (fromaddr, ", ".join(toaddr), messagetosend, Subject)
+
+# Send the mail
+
+# The actual mail send
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+
+server.starttls()
+
+server.login(username,password)
+
+server.sendmail(fromaddr, toaddr, messagetosend)
+
+server.quit()
